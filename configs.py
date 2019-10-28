e3289d6d2087856503c4a32ed60ccaea46cd62b3
Nilaksh Das <nilakshdas@gatech.edu>
2019-10-28 11:44:31 -0400

    Ignore IDE settings, update and create save directory

--- configs.py
+++ configs.py
@@ -1,6 +1,11 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+import os
+
+save_dir                    = '/home/ubuntu/data/CloserLookFewShot/'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
 data_dir['omniglot']        = './filelists/omniglot/' 
-data_dir['emnist']          = './filelists/emnist/' 
+data_dir['emnist']          = './filelists/emnist/'
+
+
+os.makedirs(save_dir, exist_ok=True)
